af05350ee2b2964ef2e79382179776d2eb397903
Razvan Takacs <sergiurazvan.takacs@gmail.com>
2023-05-16 20:03:55 +0300

    Initial commit

---
+++ src/main/java/pages/BasePage.java
@@ -0,0 +1,27 @@
+package pages;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+public class BasePage {
+    protected WebDriver driver;
+    private String baseUrl = "https://www.lambdatest.com/selenium-playground/";
+    public BasePage() {
+    }
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+    }
+    @BeforeClass
+    public void setUp() {
+        System.setProperty("web-driver.chrome.driver",
+                "Z:\\QA\\Chromadriver\\chromedriver.exe\\");
+        driver = new ChromeDriver();
+        driver.get(baseUrl);
+    }
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
+
